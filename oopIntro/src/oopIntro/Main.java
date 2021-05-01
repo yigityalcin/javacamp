2a33eb1222ed425b81b10b3e755ba2ad32766472
enginaar <enginaar@DESKTOP-K8GBU7S>
2021-05-01 17:02:19 +0300

    First Commit

---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,18 @@
+package oopIntro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Product product1 = new Product();
+		product1
+		
+		Product[] products = {};
+		
+		System.out.println(products.length);
+		
+		
+	
+	}
+
+}
